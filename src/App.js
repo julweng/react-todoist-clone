48edc221bcc226f6ad95f5b55e75d9a19459793b
julweng <julweng@gmail.com>
2020-07-08 11:05:59 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { Header } from "components/layout"
 
 export const App = () => (
-	<div className="App"></div>
+	<div className="App">
+    <Header />
+  </div>
 )
 
