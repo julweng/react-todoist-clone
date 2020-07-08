ab35adcabdd3bff05f8e20e43444e6167f4d5bb2
julweng <julweng@gmail.com>
2020-07-08 11:11:52 -0700

    content and sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { Header } from "components/layout"
+import { Content, Header } from "components/layout"
 
 export const App = () => (
 	<div className="App">
     <Header />
+    <Content />
   </div>
 )
-
---
+++ src/components/layout/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Sidebar } from "."
+
+export const Content = () => {
+  return (
+    <section>
+      <Sidebar />
+    </section>
+  )
+}
