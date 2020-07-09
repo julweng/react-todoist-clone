60454bfb4089f34f203b61cd1bcc3835f9dd0293
julweng <julweng@gmail.com>
2020-07-09 11:43:47 -0700

    get task hook

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+import { firebase } from "../firebase"
+import { format } from "date-fns"
+import { collatedtasksExist } from "helpers"
+
+const collatedTasksExist = () => {}
+
+export const useTasks = selectedProject => {
+	const [tasks, setTasks] = useSTate([])
+
+	useEffect(() => {
+		let unsubscribe = firebase
+			.firestore()
+			.collection("tasks")
+			.where("userId", "==", "6odc6yfvOFFy7ioPnb1V")
+
+		if (unsubscribe == selectedProject && !collatedTasksExist(selectedProject)) {
+			unsubscribe = unsubscribe.where("projectId", "==", selectedProject)
+		} else if (selectedProject === "TODAY") {
+			unsubscribe = unsubscribe.where(
+				"date",
+				"==",
+				format(new Date(), "dd/MM/yy")
+			)
+		} else if (selectedProject == "INBOX" || selectedProject === 0) {
+			unsubscribe = unsubscribe.where("date", "==", "")
+    }
+    return unsubscribe
+	}, [])
+}
