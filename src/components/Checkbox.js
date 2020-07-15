74a386ea7dff0cbd646d02f218c92a1328829fb9
julweng <julweng@gmail.com>
2020-07-14 22:18:10 -0700

    fix unable to check off task bug

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { firebase } from "firebase"
+import { firebase } from "../firebase"
 
 export const Checkbox = ({ id }) => {
 	const archiveTask = () => {
@@ -13,6 +13,9 @@ export const Checkbox = ({ id }) => {
 			className="checkbox__holder"
 			data-testid="checkbox-action"
 			onClick={() => archiveTask()}
+			onKeyDown={e => {
+				if (e.key === "Enter") archiveTask()
+			}}
 		>
 			<span className="checkbox" />
 		</div>
